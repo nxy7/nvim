e73cecb47672f57caba461d4eec2c05d19324c7d
nxyt <lolnoxy@gmail.com>
2022-12-28 16:46:54 +0100

    nvim-tree swapped for neo-tree

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,9 +1,11 @@
 return {
-	"rebelot/kanagawa.nvim",
+	"projekt0n/github-nvim-theme",
 	lazy = false,
-	config = {
-	},
-	init = function(LazyPlugin)
-		vim.cmd("colorscheme kanagawa")
+	config = function()
+		require("github-theme").setup({
+			theme_style = "dark",
+			dark_float = true,
+		})
+		-- vim.cmd("colorscheme github_dark")
 	end
 }
